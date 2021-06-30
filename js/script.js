17e2fd6c992798a01e65554fa3d9f256ba422c28
ashuxldr <ashuxldr@gmail.com>
2021-06-30 15:00:00 +0530

    fixed carousel

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
-const preLoader= () =>{
-  setTimeout(showContent,4000);
-}
-const showContent=()=>{
-  document.getElementById("preloader").style.display="none";
-  document.getElementById('mainContent').style.transition = 'all 2.0s ease';
-  document.getElementById('mainContent').style.display = 'block';
+const preLoader = () => {
+  setTimeout(showContent, 3000);
+};
+const showContent = () => {
+  // document.getElementById("preloader").style.visibility = "hidden";
+  document.getElementById("preloader").remove();
+  document.getElementById("mainContent").style.visibility = "visible";
+  document.getElementById("mainContent").style.transition = "all 2.0s ease";
   startTimer();
-}
-
-
-
-
+};
 
 function startTimer() {
   var images = [],
@@ -24,80 +21,3 @@ function startTimer() {
     document.getElementById("img").src = images[x];
   }, 3000);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var images = [],
-//   x = -1;
-// images[0] = "./images/02.svg";
-// images[1] = "./images/03.svg";
-// images[2] = "./images/04.svg";
-// images[3] = "./images/01.svg";
-// function displayNextImage() {
-//   x = x === images.length - 1 ? 0 : x + 1;
-//   document.getElementById("img").src = images[x];
-// }
-
-// function startTimer() {
-//   var images = [],
-//     x = -1;
-//   images[0] = "./images/02.svg";
-//   images[1] = "./images/03.svg";
-//   images[2] = "./images/04.svg";
-//   images[3] = "./images/01.svg";
-//   setInterval(function () {
-//     x = x === images.length - 1 ? 0 : x + 1;
-//     document.getElementById("img").src = images[x];
-//   }, 3000);
-// }
